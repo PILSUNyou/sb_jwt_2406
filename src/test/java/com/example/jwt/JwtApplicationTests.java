e5cd6a68e6ac771f0dd66b716496bfac323bc830
PILSUNyou <dbyo1125@naver.com>
2024-06-10 15:49:49 +0900

    hmac encode

--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.jwt;
 
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,12 +16,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JwtApplicationTests {
 
 	@Value("${custom.jwt.secretKey}")
-	private String secretKey;
+	private String secretKeyPlain;
 
 	@Test
 	@DisplayName("시크릿 키 존재 여부 체크")
 	void Test1() {
-		assertThat(secretKey).isNotNull();
+		assertThat(secretKeyPlain).isNotNull();
 	}
 
+	@Test
+	@DisplayName("secretKeyPlain을 이용하여 암호화 알고리즘 SecretKey 객체 생성")
+	void Test2() {
+		String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+		// 시크릿키를 64비트 쪼개어 다시 만들고, 시크릿키 타입을 만들고 객체를 생성 한다.
+		SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+		// 객체가 정상적으로 생성이 됐는지 확인하는 작업
+		assertThat(secretKey).isNotNull(); // NULL값인지 확인
+	}
 }
