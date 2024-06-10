07e3de926c9941d144a12a9693a3160cd9df5287
PILSUNyou <dbyo1125@naver.com>
2024-06-10 16:13:09 +0900

    jwt provider

--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.jwt;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,10 +11,14 @@ import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtApplicationTests {
+
+	@Autowired
+	private JwtPrivider jwtProvider;
 
 	@Value("${custom.jwt.secretKey}")
 	private String secretKeyPlain;
@@ -33,4 +38,20 @@ class JwtApplicationTests {
 		// 객체가 정상적으로 생성이 됐는지 확인하는 작업
 		assertThat(secretKey).isNotNull(); // NULL값인지 확인
 	}
+
+	@Test
+	@DisplayName("jwtProvider 객체를 활용하여 SercetKey 객체 생성")
+	void Test3() {
+		SecretKey secretKey = jwtProvider.getScretKey();
+		assertThat(secretKey).isNotNull();
+	}
+
+	@Test
+	@DisplayName("SecretKey 객체 생성을 한 번만 하도록 처리")
+	void Test4() {
+		SecretKey secretKey1 = jwtProvider.getScretKey();
+		SecretKey secretKey2 = jwtProvider.getScretKey();
+		assertThat(secretKey1 == secretKey2).isTrue();
+	}
+
 }
